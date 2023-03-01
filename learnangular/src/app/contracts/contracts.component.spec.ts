a34482712c720e4cc6c1162f6f5d70c7542ff277
dedi004 <99738296+dedi004@users.noreply.github.com>
2023-03-01 17:09:08 +0200

    Added uploader, contracts and overview

---
+++ learnangular/src/app/contracts/contracts.component.spec.ts
@@ -0,0 +1,32 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
+import { ContractsComponent } from './contracts.component';
+
+describe('ContractsComponent', () => {
+  let component: ContractsComponent;
+  let fixture: ComponentFixture<ContractsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContractsComponent ],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatTreeModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContractsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
